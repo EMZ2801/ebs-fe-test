fcf1cd81d031908264ade021fdf1c32d76e07af3
EMZ2801 <Ekaterina.dubei@mail.ru>
2024-12-03 18:10:22 +0300

    Add styles to product cards

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -8,7 +8,10 @@ const ProductItem: React.FC<{ product: Product }> = ({ product }) => {
   
     return (
       <div className="product-card">
-        <img src={product.image} alt={product.title} />
+        <div className='imageCart'>
+            <img src={product.image} alt={product.title} />
+        </div>
+     
         <h3>{product.title}</h3>
         <p>Цена: {product.price} руб.</p>
         
